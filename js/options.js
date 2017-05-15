91350f5e6aea39f3d901975e747b1ccf4778472d
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-15 07:21:20 -0400

    options reset button created

--- js/options.js
+++ js/options.js
@@ -3,19 +3,38 @@
 let Options = (function() {
 	let _selectLeadsCount = $("#select-leads-count"),
 		_inputRecordsCount = $("#input-records-count"),
-		_inputFilePrefix = $("#input-file-prefix");
+		_inputFilePrefix = $("#input-file-prefix"),
+		_btnReset = $("#reset");
 
 	let initializeComponents = () => {
 		let settings = LeadsFinder.settings;
 
 		for (let p in settings) {
 			let config = settings[p];
-			$("#" + config.id).val(config.value);
+			$("#" + config.id).val(JSON.parse(localStorage[p] || "null") || config.value);
 		}
 	};
 
+	let resetConfig = () => {
+		let settings = LeadsFinder.settings;
+
+		for (let p in settings) {
+			let config = settings[p];
+			$("#" + config.id).val(config.value).change();
+			localStorage[p] = JSON.stringify(config.value);
+		}
+	}
+
+	let inputChangeHandler = () => {
+		let target = event.target.getAttribute("data-target");
+		localStorage[target] = JSON.stringify(event.target.value);
+	}
+
 	let init = () => {
 		initializeComponents();
+		$("input").change(inputChangeHandler);
+		$("select").change(inputChangeHandler);
+		_btnReset.click(resetConfig);
 	}
 
 	return {
